68e23f916b40c6660542300d9991b99bae59c546
ridvan.dogan <ridvan.dogan@kolaysoft.com.tr>
2022-06-24 13:59:53 +0300

    api gateway added

---
+++ apigw/src/main/java/com/rd/apigw/ApiGWApplication.java
@@ -0,0 +1,18 @@
+package com.rd.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created at 6/24/22.
+ *
+ * @author Ridvan Dogan
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGWApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGWApplication.class, args);
+    }
+}
--- customer/src/main/java/com/rd/customer/CustomerController.java
+++ customer/src/main/java/com/rd/customer/CustomerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/customer")
+@RequestMapping("api/v1/customers")
 @AllArgsConstructor
 public class CustomerController {
 
